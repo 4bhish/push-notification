122cb3ee6ce0c1dc1d4e5aeed287d645177e5389
Abhishek Hadimani <abhihadimani143@gmail.com>
2024-01-26 14:20:48 +0530

    Update Realtime Database

--- src/notification/firebase.config.js
+++ src/notification/firebase.config.js
@@ -1,6 +1,7 @@
 
 import { initializeApp } from "firebase/app";
 import { getMessaging, getToken } from 'firebase/messaging'
+import { getDatabase, push, ref, set } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCQ0HR7Jbeo-IVgYKNFv3_cznVpCuI_Qe0",
@@ -8,36 +9,36 @@ const firebaseConfig = {
   projectId: "push-notification-9aa00",
   storageBucket: "push-notification-9aa00.appspot.com",
   messagingSenderId: "760446349143",
-  appId: "1:760446349143:web:4f8e271a5ba367a02ad812"
+  appId: "1:760446349143:web:4f8e271a5ba367a02ad812",
+  databaseURL:'https://push-notification-9aa00-default-rtdb.firebaseio.com/',
 };
 
 
 const app = initializeApp(firebaseConfig);
 export const messaging = getMessaging();
+export const db = getDatabase(app)
 
 
-// const savePermissionStatus = (status) => {
-//   localStorage.setItem('notificationPermission', status);
-// }
-
-// const getPermission = async () => {
-//   const permissionStatus = await Notification.requestPermission()
-//   savePermissionStatus(permissionStatus) 
-//   return permissionStatus
-// }
 
 
 export const generateToken = async() => {
-  // const permission = await getPermission()
 
   const permissionStatus = await Notification.requestPermission()
 
   if(permissionStatus === 'granted'){
     const token = await getToken(messaging,{vapidKey:'BHNoKuxbLEag-PXrt7SvFO7APd6DnoLs4AiOSrN9Q-r58V567Ac7mZREgd6887pYWFum3_nARc74OJPivj6yuzo'})
-
+    writeToken(token)
     console.log(token)
   } else if(permissionStatus === 'denied'){
     alert('you have denied permission pls allow in browser setting')
   }
+}
+
+function writeToken(token){
+  const tokensRef = ref(db,'token')
+
+  push(tokensRef,{
+    token:token
+  })
 }
 
